ead889537a0925b8597857561771aa6834ae16bb
Brrrandon-Lam <lamb@oregonstate.edu>
2020-06-05 20:58:40 -0700

    added quick function to server.js

--- server.js
+++ server.js
@@ -7,7 +7,14 @@ var port = process.env.PORT || 3000;
 app.engine("handlebars", expressHandlebars({defaultLayout: null}));//should change this eventually
 app.set("view engine", "handlebars");
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
+
+app.get('/', function (req, res, next) {
+    console.log("== Got a request")
+    console.log("== Request Parameters:", req.params);
+    //uses twitData.json to display information
+    res.render('index.html');
+});
 
 app.listen(port, function () {
 	console.log("Server listening on port", port);
