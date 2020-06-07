404c372e99d2edddd8823c5ca8583f190365c9a2
Dana Pearson <anad.noser@gmail.com>
2020-06-06 18:26:08 -0700

    made page and home templates and serving

--- server.js
+++ server.js
@@ -4,17 +4,18 @@ var expressHandlebars = require("express-handlebars");
 var app = express();
 var port = process.env.PORT || 3000;
 
-app.engine("handlebars", expressHandlebars({defaultLayout: null}));//should change this eventually
+app.engine("handlebars", expressHandlebars({defaultLayout: "pageTemplate"}));//should change this eventually
 app.set("view engine", "handlebars");
 
 app.use(express.static(__dirname + "/public"));
 
 app.get('/', function (req, res, next) {
-    console.log("== Got a request")
-    console.log("== Request Parameters:", req.params);
-    //uses twitData.json to display information
-    res.render('index.html');
+	res.status(200).render("homePage", {pageTitle: "Homepage"});
 });
+
+app.get("*", function (req, res, next){
+	res.status(404).render("404", {pageTitle: "404 - page not found"});
+})
 
 app.listen(port, function () {
 	console.log("Server listening on port", port);
