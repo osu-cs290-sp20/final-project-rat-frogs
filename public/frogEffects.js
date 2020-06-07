c55164b4151e8b3cf63d79a2fc3002f0c1e60144
Jacob Cheney <cheneyja@oregonstate.edu>
2020-06-07 01:24:45 -0700

    Update frogEffects.js
    Adding comments

--- public/frogEffects.js
+++ public/frogEffects.js
@@ -1,8 +1,13 @@
 
 function frogChange() {
 
+
+    //Accesses  the frog-img class object
     var frogImage = document.getElementsByClassName("frog-img");
 
+    //Returns an array so we gotta access it as such (very lame)
+    //Then, it'll add the class frogAnimate, which contains animation information from the CSS file
+    
     frogImage[0].classList.add('frogAnimate');
 
 }
