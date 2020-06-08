988d361158bada99158d3f9ba084b9faa9a1d963
Brrrandon-Lam <lamb@oregonstate.edu>
2020-06-07 23:57:51 -0700

    frog facts stuff

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ app.set("view engine", "handlebars");
 
 app.use(express.static(__dirname + "/public"));
 
+var fluteSalad = new Audio('./audio/flutesalad.mp3');
+
 app.get('/', function (req, res, next) {
 	var test = {
 		url: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/The_Green_and_Golden_Bell_Frog.jpg/1280px-The_Green_and_Golden_Bell_Frog.jpg",
@@ -17,9 +19,13 @@ app.get('/', function (req, res, next) {
 	res.status(200).render("homePage", {pageTitle: "Homepage", photos: [test, test, test]});
 });
 
+app.get('/frog-facts', function (req, res, next) {
+	res.status(200).render("frogFactsPage", { pageTitle: "Frog Facts Page" });
+});
 
-app.get("*", function (req, res, next){
-	res.status(404).render("404", {pageTitle: "404 - page not found"});
+
+app.get("*", function (req, res, next) {
+	res.status(404).render("404", { pageTitle: "404 - page not found" });
 })
 
 app.listen(port, function () {
