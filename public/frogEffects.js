aaef5eb4c31ace451452a011a31ec972af7558f3
Dana Pearson <anad.noser@gmail.com>
2020-06-10 18:02:25 -0700

    added frog fact deep fry, made 404 gif bigger

--- public/frogEffects.js
+++ public/frogEffects.js
@@ -2,12 +2,24 @@
 function frogChange() {
 
 
-    //Accesses  the frog-img class object
-    var frogImage = document.getElementsByClassName("frog-img");
+	//Accesses  the frog-img class object
+	var frogImage = document.getElementsByClassName("frog-img");
 
-    //Returns an array so we gotta access it as such (very lame)
-    //Then, it'll add the class frogAnimate, which contains animation information from the CSS file
-    
-    frogImage[0].classList.add('frogAnimate');
+	//Returns an array so we gotta access it as such (very lame)
+	//Then, it'll add the class frogAnimate, which contains animation information from the CSS file
+	
+	frogImage[0].classList.add('frogAnimate');
 
+}
+
+function deepFryFrogFactsClick(){
+	var imagesTest = ["https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/Rane_fritte_in_Italia_-_Fried_frogs_in_Italy.JPG/800px-Rane_fritte_in_Italia_-_Fried_frogs_in_Italy.JPG"];
+	
+	var imagesCurrent = document.getElementsByClassName("frog-img");
+
+	for(var x = 0; x < imagesCurrent.length; x++){
+		var replaceWith = imagesTest[x] || imagesTest[0];
+		var currentImg = imagesCurrent[x];
+		currentImg.src = replaceWith;
+	}
 }
